86d4e2340143d4c6fbfd94bee4cdc2a57341057a
Bing866 <114146062+Bing866@users.noreply.github.com>
2022-11-11 05:06:13 +0800

    期中完成

--- 2DGame_109051361airplane/Assets/Scripts/DamageSystem.cs
+++ 2DGame_109051361airplane/Assets/Scripts/DamageSystem.cs
@@ -11,5 +11,34 @@ namespace BING
         private GameObject prefabExplosion;
         [SerializeField, Header("碰到會爆炸的名稱")]
         private string nameTarget;
+
+        // 碰撞開始時執行一次
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            print(collision.gameObject.name);
+
+            // 如果 碰到的物件名稱 包含 nameTarget 就爆炸並刪除
+            if (collision.gameObject.name.Contains(nameTarget))
+            {
+                // 生成爆炸預製物 座標角度 跟此物件相同
+                Instantiate(prefabExplosion, transform.position, transform.rotation);
+
+                // 刪除 Destroy
+                // 此物件 gameObject
+                Destroy(gameObject);
+            }
+        }
+
+        // 碰撞離開時執行一次
+        private void OnCollisionExit2D(Collision2D collision)
+        {
+            
+        }
+
+        // 碰撞時持續執行
+        private void OnCollisionStay2D(Collision2D collision)
+        { 
+        
+        }
     }
 }
